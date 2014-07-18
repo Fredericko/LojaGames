f470275b6ab5f256fffcba92e8f7f1bd614bef59
Geshner <geeshner@gmail.com>
2014-07-17 21:24:30 -0400

    Teste do primeiro commit e pull request

--- src/vo/PresenteVO.java
+++ src/vo/PresenteVO.java
@@ -1,7 +1,7 @@
 package vo;
 
 import java.util.List;
-/** Modificação  */
+/** Modificação  testando aqui o pull request*/
 public class PresenteVO {
 	private UsuarioVO comprador, presenteado;
 	private JogoVO jogo;
